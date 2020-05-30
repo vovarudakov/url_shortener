97a2735a68052ebf76fc4e34763c9b4187f13a45
Volodymyr Rudakov <volodymyr.rudakov@vaimo.com>
2020-05-30 17:44:06 +0200

    prepare to prod

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const config = require("config");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 
@@ -8,8 +9,16 @@ app.use(express.json({ extended: true }));
 
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/link", require("./routes/link.routes"));
+app.use("/t", require("./routes/redirect.routes"));
 
-const PORT = config.get("port") || 5000;
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client", "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = config.get("port") || 3001;
 
 async function start() {
   try {
